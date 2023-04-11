0ea174827745f620402cb6a767e5a496648c92e7
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-12 02:23:30 +0800

    feat: Add display books

--- public/script.js
+++ public/script.js
@@ -6,17 +6,53 @@ function Book(title, author, numOfPages) {
   this.numOfPages = numOfPages;
 }
 
-odinLibrary.push(new Book('Title', 'Author', '32'));
+odinLibrary.push(new Book('The Alchemist', 'Paulo Coelho', '167'));
+odinLibrary.push(new Book('The Great Gatsby', 'F. Scott Fitzgerald', '184'));
 
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const numOfPages = document.querySelector('#pages');
-const addButton = document.querySelector('#addButton');
+const titleField = document.querySelector('#title');
+const authorField = document.querySelector('#author');
+const numOfPagesField = document.querySelector('#pages');
+const addButton = document.querySelector('#add-button');
+const bookList = document.querySelector('#book-list');
+
+const createBookCard = (book) => {
+  const card = document.createElement('div');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h4');
+  const bookPages = document.createElement('p');
+
+  card.classList.add('card');
+  bookTitle.classList.add('bookTitle');
+  bookAuthor.classList.add('bookAuthor');
+  bookPages.classList.add('bookPages');
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPages.textContent = book.numOfPages;
+
+  card.append(bookTitle, bookAuthor, bookPages);
+
+  bookList.appendChild(card);
+};
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
 
-  const book = new Book(title.value, author.value, numOfPages.value);
+  const book = new Book(
+    titleField.value,
+    authorField.value,
+    numOfPagesField.value
+  );
 
   odinLibrary.push(book);
+
+  titleField.value = '';
+  authorField.value = '';
+  numOfPagesField.value = '';
+
+  createBookCard(book);
+});
+
+odinLibrary.forEach((book) => {
+  createBookCard(book);
 });
